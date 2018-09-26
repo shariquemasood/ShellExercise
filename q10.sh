27317a5e0cf342afe5fd636169c2ebd76a7a1f18
Sharique masood <43162614+shariquemasood@users.noreply.github.com>
2018-09-26 15:18:22 +0530

    Add files via upload

--- q10.sh
+++ q10.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo
-echo NAME SIZE
-ls -l | awk '{print $9,$5}'
+echo "NAME 		SIZE"
+ls -l | awk '{print $9,$5}' | sed -r -e 's/ {5}/\t/g' -e 's/ /\t/g' | sed -e 's/\t/_|/g'|  column -t -s '_'
 
 
